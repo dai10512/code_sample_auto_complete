ce4eed4cc08abd760d65b5e3e422a382ca4d9235
Daisuke Osanai <dsk.osanai@gmail.com>
2022-11-23 11:27:18 +0900

    build by some os

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +10,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AutoFillComplete Sample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('AutoFillComplete Demo'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(24.0),
+//         child: Column(
+//           children: [
+//             TextFormField(
+//               decoration: const InputDecoration(labelText: "Password"),
+//               obscureText: true,
+//               autofillHints: const [AutofillHints.password], //PWの自動入力
+//             ),
+//             const SizedBox(height: 20),
+//             MaterialButton(
+//                 child: const Text("data"),
+//                 onPressed: () {
+//                   Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                           // （2） 実際に表示するページ(ウィジェット)を指定する
+//                           builder: (context) => const NextPage()));
+//                 })
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {},
+          child: const Text('戻る'),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -30,22 +79,45 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: AutofillGroup( //複数項目をセットで保存する場合に使用
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: "e-mail"),
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: const [AutofillHints.email], //emailの自動入力
+
+        //ここからが複数データのフォームを利用する場合のコード
+
+        child: Column(
+          children: [
+            AutofillGroup(
+              //複数項目をセットで保存する場合に使用
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "e-mail"),
+                    keyboardType: TextInputType.emailAddress,
+                    autofillHints: const [AutofillHints.email], //emailの自動入力
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Password"),
+                    obscureText: true,
+                    autofillHints: const [AutofillHints.password], //PWの自動入力
+                    onEditingComplete: ()=>TextInput.finishAutofillContext(),
+                  ),
+                ],
               ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "Password"),
-                obscureText: true,
-                autofillHints: const [AutofillHints.password],  //PWの自動入力
-              ),
-            ],
-          ),
+            ),
+            MaterialButton(
+              child: const Text("data"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    // （2） 実際に表示するページ(ウィジェット)を指定する
+                    builder: (context) => const NextPage(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
+
+        // ここまで
       ),
     );
   }
